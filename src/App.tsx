941e2b00339eadd78f9ba86954b3c61de1bec64b
Your Name <rafael.amorim.praia@gmail.com>
2022-09-14 10:17:28 -0300

    aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Formulario from './Components/Formulario';
+import Lista from './Components/Lista';
+
+
 function App() {
   return (
     <div className="App">
-    <Formulario /> 
-   
+    <Formulario />
+    <Lista />
     </div>
   );
 }
