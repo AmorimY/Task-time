e0ca3f1aa25ffd784c0155a692afa4b50bcc3d88
AmorimY <rafael.amorim.praia@gmail.com>
2022-09-16 23:32:27 -0300

    computador casa

--- src/Components/botao/index.tsx
+++ src/Components/botao/index.tsx
@@ -1,12 +1,15 @@
+import { type } from 'os';
 import React from 'react';
  import style from "./botao.module.scss"
 
 class Button extends React.Component <{
-    children?: React.ReactNode
+    children?: React.ReactNode,
+    type?: "button" | "submit" | "reset" | undefined
 }> {
     render() {
+        const {type ="button"} = this.props
         return (
-            <button className={style.botao}>
+            <button type = {type}className={style.botao}>
                 {this.props.children}
             </button>
         )
